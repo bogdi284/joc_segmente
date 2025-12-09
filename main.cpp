16eca053264a221fcc2d350adcedb9c94db6ae99
bogdi284 <bogdanminia166@outlook.com>
2025-12-09 23:04:51 +0200

    - am adaugat meniul de selectare a modului de joc , accesibil din butonul start din meniul principal . - am micsorat butoanele

--- main.cpp
+++ main.cpp
@@ -8,15 +8,19 @@
 #define W  1366
 #define H 768
 #define CULOARE_FUNDAL BLACK
+#define FONT SANS_SERIF_FONT
 
 #define BUTON_CULOARE BLACK
 #define BUTON_CULOARE_HOVER YELLOW
 #define BUTON_CULOARE_TEXT WHITE
 #define BUTON_FONT SANS_SERIF_FONT
-#define BUTON_FONT_SIZE 8
+#define BUTON_FONT_SIZE 5
+#define WB 220
+#define HB 50
 
 enum stare {
 	MENIU_PRINCIPAL,
+	MENIU_MOD_JOC,
 	INSTRUCTIUNI,
 	JOC,
 	SETARI
@@ -27,7 +31,11 @@ struct buton {
 };
 
 buton buton_start, buton_setari, buton_instr, buton_iesire, buton_inapoi ;
+buton buton_pvp, buton_pvb;
+
 int stare_curenta = MENIU_PRINCIPAL;
+int mod_joc = 0; //0 - pvp si 1 - pvb
+
 // interfata (butoane)
 void init_buton(buton* b, int x, int y, int w, int h, const char* text) {
 	b->x = x;
@@ -64,8 +72,8 @@ bool este_click_pe_buton(buton b, int mx, int my) {
 }
 //interfata (meniuri)
 void init_meniu_principal() {
-	int wb = 300;
-	int hb = 60; 
+	int wb = WB;
+	int hb = HB; 
 	int xcentru = (W - wb) / 2;
 	int ystart = 250;
 	int spatiu = 90;
@@ -90,8 +98,8 @@ void desen_meniu_principal() {
 	desen_buton(buton_iesire);
 }
 void init_instructiuni() {
-	int wb = 200;
-	int hb = 50;
+	int wb = WB;
+	int hb = HB;
 	init_buton(&buton_inapoi, (W - wb) / 2, H - 100, wb, hb, "INAPOI");
 }
 void desen_instructiuni() {
@@ -120,11 +128,37 @@ void desen_instructiuni() {
 
 	desen_buton(buton_inapoi);
 }
+void init_meniu_mod_joc() {
+	int wb = WB;
+	int hb = HB;
+	int xcentru = (W - wb) / 2;
+	int ystart = 250;
+	int spatiu = 100;
+
+	init_buton(&buton_pvp, xcentru, ystart, wb, hb, "PLAYER VS PLAYER");
+	init_buton(&buton_pvb, xcentru, ystart + spatiu, wb, hb, "PLAYER VS BOT");
+
+	init_buton(&buton_inapoi, (W - 200) / 2, H - 150, 200, 50, "INAPOI");
+}
+void desen_meniu_mod_joc() {
+	setbkcolor(CULOARE_FUNDAL);
+	cleardevice();
+
+	setcolor(YELLOW);
+	settextstyle(BOLD_FONT, HORIZ_DIR, 5);
+	char titlu[] = "SELECTARE MOD DE JOC";
+	outtextxy((W - textwidth(titlu)) / 2, 100, titlu);
+	
+	desen_buton(buton_pvp);
+	desen_buton(buton_pvb);
+	desen_buton(buton_inapoi);
+}
 int main() {
 	initwindow(W , H , "Joc Segmente");
 
 	init_meniu_principal();
 	init_instructiuni();
+	init_meniu_mod_joc();
 
 	int pagina = 0;
 	bool ruleaza = true;
@@ -139,6 +173,9 @@ int main() {
 		else if (stare_curenta == INSTRUCTIUNI) {
 			desen_instructiuni();
 		}
+		else if (stare_curenta == MENIU_MOD_JOC) {
+			desen_meniu_mod_joc();
+		}
 
 		if (ismouseclick(WM_LBUTTONDOWN)) {
 			int mx, my;
@@ -151,14 +188,30 @@ int main() {
 				else if (este_click_pe_buton(buton_instr, mx, my)) {
 					stare_curenta = INSTRUCTIUNI;
 				}
-				///aici va veni start si setari :))
+				else if (este_click_pe_buton(buton_start, mx, my)) {
+					stare_curenta = MENIU_MOD_JOC;
+				}
+				///aici va veni setari :))
+			}
+			else if (stare_curenta == MENIU_MOD_JOC) {
+				if (este_click_pe_buton(buton_inapoi, mx, my)) {
+					stare_curenta = MENIU_PRINCIPAL;
+				}
+				else if (este_click_pe_buton(buton_pvp, mx, my)) {
+					mod_joc = 0; // player vs player
+					//aici va veni start joc
+					//stare_curenta = joc_pvp
+				}
+				else if (este_click_pe_buton(buton_pvb, mx, my)) {
+					mod_joc = 1; // player vs bot
+				}
 			}
 			else if (stare_curenta == INSTRUCTIUNI) {
 				if (este_click_pe_buton(buton_inapoi, mx, my)) {
 					stare_curenta = MENIU_PRINCIPAL;
 				}
 			}
-			/// aici va veni start si setari
+			/// aici va veni setari
 		}
 		pagina = 1 - pagina;
 		delay(30);
