798b047288280113770eb18593c8529b10700b98
bogdi284 <bogdanminia166@outlook.com>
2025-12-13 20:47:08 +0200

    miau miau

---
+++ backend.cpp
@@ -0,0 +1,5 @@
+#include "backend.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
---
+++ backend.h
@@ -0,0 +1,5 @@
+#pragma once
+#ifndef BACKEND_H
+#define BACKEND_H
+
+#endif // !BACKEND_H
